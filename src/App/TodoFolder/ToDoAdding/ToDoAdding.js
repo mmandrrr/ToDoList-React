12ae1b32779f10c6ecb085819b0896836a28e9bf
Maxim <mmandrrr@gmail.com>
2022-08-10 13:14:36 +0300

    goals adding logic added(click + enter button)

--- src/App/TodoFolder/ToDoAdding/ToDoAdding.js
+++ src/App/TodoFolder/ToDoAdding/ToDoAdding.js
@@ -1,12 +1,55 @@
+import { render } from '@testing-library/react'
+import { Component } from 'react'
+
 import './todoAdding.css'
 
-const ToDoAdding = (props) => {
-    return(
-        <div className="goal_add">
-            <input type="text" className="goal_add-input" placeholder="Write a task..."/>
-            <button onClick={() => props.addTask(props.tasksArr,'hello')}>Click</button>
-        </div>
-    )
+class ToDoAdding extends Component  {
+    constructor(props) {
+        super(props)
+        this.state = {
+            value : ''
+        }
+    }
+
+    inputValue = (e) => {
+        this.setState({
+            value : e.target.value
+        })
+    }
+
+    clearInput = () => {
+        this.setState({
+            value:""
+        })
+    }
+
+    onEnterPress = (e,result) => {
+        if(e.code === 'Enter') {
+            result()
+        } else {
+            return ''
+        }
+    }
+
+    render() {
+        return(
+            <div className="goal_add">
+                <input 
+                    onKeyDown={(e) => this.onEnterPress(e,() => {
+                    this.props.addTask(this.props.tasksArr,this.state.value)
+                    this.clearInput()
+                })}
+                    onChange={this.inputValue} 
+                    type="text" className="goal_add-input" 
+                    placeholder="Write a task..." 
+                    value={this.state.value}/>
+                <button onClick={() => {
+                    this.props.addTask(this.props.tasksArr,this.state.value)
+                    this.clearInput()
+                }}>Click</button>
+            </div>
+        )
+    }
 }
 
 export default ToDoAdding
