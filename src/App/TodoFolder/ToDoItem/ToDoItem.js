b2c044de1f4a0cb5d689d9fce76d1915f85ba93e
Maxim <mmandrrr@gmail.com>
2022-08-11 12:15:34 +0300

    added checkBox logic and text style

--- src/App/TodoFolder/ToDoItem/ToDoItem.js
+++ src/App/TodoFolder/ToDoItem/ToDoItem.js
@@ -1,14 +1,49 @@
+import { Component } from 'react';
+
 import './todoItem.css';
 import checked from '../../../img/checkMark.svg'
 
-const TodoItem = (props) => {
-    return(
-        <div className="goal_item">
-            <div className="goal_item-checbox"></div>
-            <img src={checked} alt="" className="goal_item-checbox-checked hide" />
-            <div className="goal_item-text">{props.itemText}</div>  
-        </div>
-    )
+class TodoItem extends Component {
+    constructor(props) {
+        super(props)
+        this.state = {
+            checkBoxClasses : 'goal_item-checbox-checked',
+            itemTextClasses : 'goal_item-text'
+        }
+    }
+
+    onCheckUncheckedBox = () => {
+        this.setState({
+            checkBoxClasses : this.state.checkBoxClasses + ' showed',
+            itemTextClasses : this.state.itemTextClasses + ' done'
+        })
+        console.log(this.state.checkBoxClasses);
+    }
+
+    onCheckCheckedBox = () => {
+        this.setState({
+            checkBoxClasses : this.state.checkBoxClasses.replace(/ showed/ig,''),
+            itemTextClasses : this.state.itemTextClasses.replace(/ done/ig,'')
+        })
+    }
+
+
+    render() {
+        return(
+            <div className="goal_item">
+                <div
+                    onClick={this.onCheckUncheckedBox} 
+                    className="goal_item-checbox"
+                ></div>
+                <img 
+                    onClick={this.onCheckCheckedBox}
+                    src={checked} alt="" 
+                    className={this.state.checkBoxClasses} 
+                />
+                <div className={this.state.itemTextClasses}>{this.props.itemText}</div>  
+            </div>
+        )
+    }
 }
 
 export default TodoItem
